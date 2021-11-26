9b236d6566c96ea04b37c483a98c72fc63542f4b
katerina2018 <tvorchestvo15@gmail.com>
2021-11-26 14:50:17 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+};
 
+refs.start.addEventListener('click', startAction);
+refs.stop.addEventListener('click', stopAction);
+
+let timeId = null;
+
+function startAction() {
+    timeId = setInterval(() => {
+        const currentColor = getRandomHexColor();
+        document.body.style.backgroundColor = currentColor;
+        console.log(currentColor);
+    }, 1000);
+
+    refs.start.setAttribute('disabled', '');
+}
+
+function stopAction() {
+    clearInterval(timeId);
+
+    refs.start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
